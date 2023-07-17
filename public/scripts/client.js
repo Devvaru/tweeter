6522a7a66c89019854a98673f40b6a9e3c471906
Devvaru <syeraru@gmail.com>
2023-07-16 22:14:31 -0600

    Remove redundant code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-$(document).ready(function(e) {
+$(document).ready(function() {
 
   const createTweetElement = function(tweet) {
     const $tweet = $(`
@@ -69,8 +43,6 @@ $(document).ready(function(e) {
     }
   };
 
-;
-
   $('#tweetform').on('submit', function(e) {
 
     // prevents default actions of form submission
@@ -101,8 +73,8 @@ $(document).ready(function(e) {
       success: (response) => {
         renderTweets(response);
       }
-    })
-  }
+    });
+  };
 
   loadtweets();
 
